50edf54b9ee67dac6734a5350b23cea48caaf179
danielder10 <dder10@my.bcit.ca>
2025-01-19 01:04:36 -0800

    temp files for some project layouts

---
+++ app/drinks/page.tsx
@@ -0,0 +1,9 @@
+import ReachOut from "@/components/ReachOut";
+
+export default function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <ReachOut />
+    </div>
+  );
+}
