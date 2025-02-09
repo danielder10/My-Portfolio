e5a1f22dfccaf317eb154bfbf2d6643762a5499e
danielder10 <dder10@my.bcit.ca>
2025-02-09 15:12:19 -0800

    completed flick cafe project

---
+++ app/flickcafe/page.tsx
@@ -0,0 +1,9 @@
+import FlickCafe from "@/components/FlickCafe";
+
+export default function Layout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <FlickCafe />
+    </div>
+  );
+}
